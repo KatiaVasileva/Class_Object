405944b7f53e9e02751bfb11bf7c677f0404c75c
katiavasileva <eka.vasileva@gmail.com>
2022-12-12 20:56:10 +0500

    Task is done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Person sarah = new Person("Sarah", 30);
+        System.out.println(sarah.getName() + " " + sarah.getAge());
+        sarah.setAge(31);
+        System.out.println(sarah.getName() + " " + sarah.getAge());
+        Person john = new Person("John", 13);
+        System.out.println(john.getName() + " " + john.getAge());
+
     }
 }
